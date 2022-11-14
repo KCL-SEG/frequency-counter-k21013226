b09ebd358dc4c5fec58f7292257c0ce9cb22cc6a
jasdeep panum <jasdeep.panum@kcl.ac.uk>
2022-11-14 12:57:21 +0000

    add code to frequency file

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,41 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+#produces dictionary based on items list
+#each item in items must contain a key
+#if item is not a string then the key = item converted to string
+#value = positive integer counting the no of times the item represented by key occurs in items
+
+
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    #iterate through each element in items
+    #for each item check if string if not convert
+
+    #check dictionary is key appears
+    #if it does not than add key with value 1
+    #if it does then update the dictionary value by 1
+
+    itemElement = ""
+
+    for x in items:
+        if type(items[x]) != str:
+            itemElement = str(items[x])
+            print("no string")
+        else:
+            itemElement = items[x]
+            print("string")
+
+        for y in frequencies:
+            if itemElement == frequencies[y]:
+                ElementCount = frequencies.get(y) + 1
+                frequencies.update({itemElement,ElementCount})
+                print(frequencies[x])
+            else:
+                frequencies[items[x]] = 1
+                print(frequencies[x])
+
+
+
     return frequencies
